83788bab5f8829da40bf76ead7bea9eef079afe4
trytomatie <trytomatie@gmail.com>
2022-01-18 17:18:03 +0100

    Fixed Alpha from Case, Fixed some UVs, Added some Interface functionality, Imrpoved ColorSwapping

--- RealtimeProject/Assets/ColorManager.cs
+++ RealtimeProject/Assets/ColorManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ColorManager : MonoBehaviour
 {
@@ -12,6 +13,8 @@ public class ColorManager : MonoBehaviour
 
     private float minAnimValue = -0.05f;
     private float maxAnimValue = 1.05f;
+
+    private bool interuptAnimaton = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,14 +27,26 @@ public class ColorManager : MonoBehaviour
         
     }
 
+
+    public void ChangeColor(Image image)
+    {
+        Color newColor = image.color;
+        interuptAnimaton = true;
+        StartCoroutine(ChangeColorAnimation(newColor));
+    }
     public void ChangeColor(Color newColor)
     {
-
-            StartCoroutine(ChangeColorAnimation(newColor));
+        interuptAnimaton = true;
+        StartCoroutine(ChangeColorAnimation(newColor));
     }
 
     IEnumerator ChangeColorAnimation(Color newColor)
     {
+        if(interuptAnimaton == true)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        interuptAnimaton = false;
         float value = metalMaterials[0].GetFloat("_ColorChangeAnimationValue");
         int direction = 1;
         if(value > 0.5f)
@@ -63,7 +78,7 @@ public class ColorManager : MonoBehaviour
             i++;
             yield return new WaitForEndOfFrame();
         }
-        while (i < 400 && value != maxAnimValue && value != maxAnimValue);
+        while (i < 400 && value != maxAnimValue && value != maxAnimValue && !interuptAnimaton);
     }
 
     [CustomEditor(typeof(ColorManager))]
