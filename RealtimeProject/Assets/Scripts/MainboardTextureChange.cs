0b148a447dc8991c0d22678867485f3147fe0989
MarkusSchwalb <96125033+MarkusSchwalb@users.noreply.github.com>
2022-01-26 18:52:16 +0100

    update Ram interface
    ich räume das nachher noch weiter auf und ich schaue auch ob ich das mit der RGB zum laufen bekomme

--- RealtimeProject/Assets/Scripts/MainboardTextureChange.cs
+++ RealtimeProject/Assets/Scripts/MainboardTextureChange.cs
@@ -14,19 +14,20 @@ public class MainboardTextureChange : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //setze zum start die Textur auf die Textur mit dem Index 0
         ChangeMaterial(0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow)==true)
+       /* if (Input.GetKeyDown(KeyCode.UpArrow)==true)
         {
             ChangeMaterial(basecolorIndex);
-        }
+        }*/
     }
 
-
+    //verändere die Basecolor des Mainboards zu dem Index
     public void ChangeMaterial(int textureIndex)
     {
         mainboardShader.SetTexture("_BaseColor", baseColorTexture[textureIndex]);
